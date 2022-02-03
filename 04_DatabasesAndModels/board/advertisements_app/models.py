0d08eb9703589460bc125ba3e6d2c002ed1953e6
@kozminskii_fedor <fvk.1972@gmail.com>
2022-02-03 23:57:22 +0300

    Задание 04- База данных и модели

--- 04_DatabasesAndModels/board/advertisements_app/models.py
+++ 04_DatabasesAndModels/board/advertisements_app/models.py
@@ -1,2 +1,61 @@
 from django.db import models
+from datetime import datetime
+from pycbrf.toolbox import ExchangeRates
 
+date_now = datetime.now().strftime('%Y-%m-%d')
+rates = ExchangeRates(date_now)['USD'].rate
+
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=1000,
+                             verbose_name='заголовок объявления')
+    description = models.CharField(max_length=1000, default='',
+                                   verbose_name='описание')
+    price = models.IntegerField(default=0, verbose_name='цена')
+    publication_date = models.DateTimeField(auto_now_add=True,
+                                            verbose_name='дата публикации')
+    publication_end_date = models.DateField(
+        verbose_name='дата окончания публикации')
+    views_count = models.IntegerField(default=0,
+                                      verbose_name='количество просмотров')
+    user_adv = models.ForeignKey('AdvertisementUser', default=None, null=True,
+                                 on_delete=models.CASCADE, blank=True,
+                                 verbose_name='автор объявления')
+    category_adv = models.ForeignKey('AdvertisementCategory', default=None,
+                                     null=True,
+                                     on_delete=models.CASCADE,
+                                     verbose_name='Рубрика')
+    type_adv = models.ForeignKey('AdvertisementType', default=None, null=True,
+                                 on_delete=models.CASCADE,
+                                 verbose_name='Тип объявления')
+
+    def get_price_usd(self):
+        return round(self.price / rates, 2)
+
+    price_usd = property(get_price_usd)
+
+    def __str__(self):
+        return self.title
+
+
+class AdvertisementUser(models.Model):
+    name = models.CharField(max_length=100, verbose_name='имя')
+    e_mail = models.EmailField(verbose_name='E-mail')
+    phone = models.IntegerField(verbose_name='телефон')
+
+    def __str__(self):
+        return self.name
+
+
+class AdvertisementCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class AdvertisementType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,1 +1,37 @@
-from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.shortcuts import render
+from django.views import generic
+from django.views.generic import TemplateView
+from .models import Advertisement
+
+
+class About(TemplateView):
+    template_name = 'advertisements_app/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Бесплатные объявления'
+        context['name'] = 'ООО "Пайтон адвертисментс"'
+        context['description'] = """
+        Расскажите о себе. Найдите поставщиков. Расширьте связи. 
+        Сосредоточьтесь на бизнесе. Все необходимые инструменты доступны 
+        сразу после регистрации.
+        """
+        return context
+
+
+class AdvertisementListView(generic.ListView):
+    model = Advertisement
+    template_name = 'advertisement_list.html'
+    context_object_name = 'advertisement_list'
+
+
+class AdvertisementDetailView(generic.DetailView):
+    model = Advertisement
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.views_count += 1
+        self.object.save()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
