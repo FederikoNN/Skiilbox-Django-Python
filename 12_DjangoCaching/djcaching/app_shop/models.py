59b1d44daa199fb8343444be6daad256179dc2d9
@kozminskii_fedor <fvk.1972@gmail.com>
2022-07-26 18:19:32 +0300

    Задание 12- Кэширование (i18n админки)

--- 12_DjangoCaching/djcaching/app_shop/models.py
+++ 12_DjangoCaching/djcaching/app_shop/models.py
@@ -8,10 +8,18 @@ class Balance(models.Model):
     balance = models.IntegerField(default=0, null=True,
                                   verbose_name=_('balance'))
 
+    class Meta:
+        verbose_name = _('balance')
+        verbose_name_plural = _('balances')
+
 
 class Promo(models.Model):
     title = models.CharField(max_length=100, verbose_name=_('title promo'))
     terms = models.CharField(max_length=1000, verbose_name=_('terms of promo'))
+
+    class Meta:
+        verbose_name = _('promotion')
+        verbose_name_plural = _('promotions')
 
 
 class Offer(models.Model):
@@ -22,6 +30,10 @@ class Offer(models.Model):
                                    verbose_name=_('offer to user'),
                                    related_name='user')
 
+    class Meta:
+        verbose_name = _('offer')
+        verbose_name_plural = _('offers')
+
 
 class Purchase(models.Model):
     name = models.CharField(max_length=100, verbose_name=_('purchase name'))
@@ -31,7 +43,15 @@ class Purchase(models.Model):
                                       verbose_name=_('buyer'),
                                       related_name='buyer')
 
+    class Meta:
+        verbose_name = _('purchase')
+        verbose_name_plural = _('purchases')
+
 
 class Shop(models.Model):
     name = models.CharField(max_length=100, verbose_name=_('shop name'))
     address = models.CharField(max_length=150, verbose_name=_('shop address'))
+
+    class Meta:
+        verbose_name = _('shop')
+        verbose_name_plural = _('shops')
