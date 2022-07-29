f88137c0818f6febfe07762ddfdb1b25fc7ac5c0
@kozminskii_fedor <fvk.1972@gmail.com>
2022-07-29 11:41:57 +0300

    Задание 13- DRF change pagination (доработка)

--- 13_IntroductionToDjangoRESTFramework/djlibrary/app_books/views.py
+++ 13_IntroductionToDjangoRESTFramework/djlibrary/app_books/views.py
@@ -1,44 +1,39 @@
 from rest_framework.mixins import ListModelMixin
 from rest_framework.generics import GenericAPIView
-
+from django_filters.rest_framework import FilterSet, DjangoFilterBackend
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
 
 class AuthorList(ListModelMixin, GenericAPIView):
+    queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-
-    def get_queryset(self):
-        queryset = Author.objects.all()
-        author_name = self.request.query_params.get('first_name')
-        if author_name:
-            queryset = queryset.filter(first_name=author_name)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['first_name']
 
     def get(self, request):
         return self.list(request)
 
 
+class BooksFilter(FilterSet):
+    class Meta:
+        model = Book
+        fields = {
+            'title': ['exact'],
+            'pages_number': ['gt', 'exact', 'lt']
+        }
+
+
 class BookList(ListModelMixin, GenericAPIView):
     serializer_class = BookSerializer
+    filterset_class = BooksFilter
 
     def get_queryset(self):
         queryset = Book.objects.all()
-        pages = self.request.query_params.get('pages_number')
-        title = self.request.query_params.get('title')
         author = self.request.query_params.get('authors')
-        if pages:
-            # Реализация фильтра "равно"
-            # queryset = queryset.filter(pages_number=pages)
-            #
-            # Реализация фильтра "меньше"
-            # queryset = queryset.filter(pages_number__lt=pages)
-            #
-            # Реализация фильтра "больше"
-            queryset = queryset.filter(pages_number__gt=pages)
-        elif author and title:
+        if author:
             author = Author.objects.filter(last_name=author).first()
-            queryset = queryset.filter(authors=author, title=title)
+            queryset = queryset.filter(authors=author)
         return queryset
 
     def get(self, request):
--- 13_IntroductionToDjangoRESTFramework/djlibrary/djlibrary/settings.py
+++ 13_IntroductionToDjangoRESTFramework/djlibrary/djlibrary/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'app_books',
 ]
@@ -105,6 +106,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'],
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
